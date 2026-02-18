9727c9724f50c434495a55f0939fcaf50fb75cca
Gemma <gcarravi@gmail.com>
2026-02-18 15:52:52 +0000

    Completed About Me page

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-+%rwfb45a87gi21b#ytjvhen#x(x9afr$6m0)8tfaq6a-a3of$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_summernote',
     'blog',
+    'django_summernote',
     'about',
 ]
 
